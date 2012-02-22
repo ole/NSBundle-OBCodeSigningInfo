857d4aa7fd8cff8c6d0a3eeaf7bb96718536576c
Ole Begemann <ole@oleb.net>
2012-02-22 10:05:18 +0100

    Add initial blank command line project

---
+++ EnumerateApps/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EnumerateApps
+//
+//  Created by Ole Begemann on 22.02.12.
+//  Copyright (c) 2012 Ole Begemann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
