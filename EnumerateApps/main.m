620af616ae69bd8d185c5f170d1640d630cc360b
Ole Begemann <ole@oleb.net>
2012-02-22 16:33:24 +0100

    Output info about specific bundles if app launched with command line argument(s)

--- EnumerateApps/main.m
+++ EnumerateApps/main.m
@@ -14,47 +14,85 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool 
     {
-        NSFileManager *fileManager = [[NSFileManager alloc] init];
-        NSArray *appDirectories = [fileManager URLsForDirectory:NSApplicationDirectory inDomains:NSAllDomainsMask];
-        
-        for (NSURL *directory in appDirectories) 
+        if (argc > 1)
         {
-            NSString *directoryPath = [directory path];
-            BOOL fileExists = [fileManager fileExistsAtPath:directoryPath];
-            if (!fileExists) {
-                NSLog(@"Skipping %@ because it does not exist.", directoryPath);
-                continue;
-            }
-            
-            NSArray *propertiesToCache = [NSArray arrayWithObjects:NSURLNameKey, NSURLTypeIdentifierKey, nil];
-            NSDirectoryEnumerator *directoryEnumerator = [fileManager enumeratorAtURL:directory includingPropertiesForKeys:propertiesToCache options:NSDirectoryEnumerationSkipsPackageDescendants errorHandler:^BOOL(NSURL *url, NSError *error) {
-                
-                NSLog(@"Error enumerating %@ in %@: %@", url, directory, error);
-                return YES;
-            }];
-            
-            for (NSURL *url in directoryEnumerator) 
+            for (unsigned int argumentIndex = 1; argumentIndex < argc; argumentIndex++)
             {
-                NSDictionary *resourceValues = [url resourceValuesForKeys:propertiesToCache error:NULL];
-                NSString *uti = [resourceValues objectForKey:NSURLTypeIdentifierKey];
+                const char *argument = argv[argumentIndex];
+                NSString *bundlePath = [NSString stringWithCString:argument encoding:NSUTF8StringEncoding];
+                NSString *appName = [bundlePath lastPathComponent];
+                NSBundle *appBundle = [NSBundle bundleWithPath:bundlePath];
                 
-                if ([uti isEqualToString:@"com.apple.application-bundle"]) 
-                {
-                    NSString *appName = [resourceValues objectForKey:NSURLNameKey];
-                    NSBundle *appBundle = [[NSBundle alloc] initWithURL:url];
-
-                    if ([appBundle comesFromAppStore]) {
-                        NSLog(@"%@ is from the Mac App Store.", appName);
-                    }
+                BOOL comesFromAppStore = [appBundle comesFromAppStore];
+                OBCodeSignState codeSignState = [appBundle codeSignState];
+                if (comesFromAppStore) {
+                    NSLog(@"%@ is from the Mac App Store.", appName);
+                }
+                if ((codeSignState & OBCodeSignStateUnsigned) == OBCodeSignStateUnsigned) {
+                    NSLog(@"%@ is not code signed.", appName);
+                }
+                if ((codeSignState & OBCodeSignStateSignatureValid) == OBCodeSignStateSignatureValid) {
+                    NSLog(@"%@ has a valid code signature.", appName);
+                }
+                if ((codeSignState & OBCodeSignStateSignatureInvalid) == OBCodeSignStateSignatureInvalid) {
+                    NSLog(@"%@ has an invalid code signature.", appName);
+                }
+                if ((codeSignState & OBCodeSignStateSignatureUnsupported) == OBCodeSignStateSignatureUnsupported) {
+                    NSLog(@"%@ has an unsupported code signature.", appName);
+                }
+                if ((codeSignState & OBCodeSignStateSignatureNotVerifiable) == OBCodeSignStateSignatureNotVerifiable) {
+                    NSLog(@"%@'s signature could not be verified.", appName);
+                }
+                if ((codeSignState & OBCodeSignStateSandboxed) == OBCodeSignStateSandboxed) {
+                    NSLog(@"%@ is sandboxed.", appName);
+                } else {
+                    NSLog(@"%@ is not sandboxed.", appName);
+                }
+            }
+        }
+        else
+        {
+            NSFileManager *fileManager = [[NSFileManager alloc] init];
+            NSArray *appDirectories = [fileManager URLsForDirectory:NSApplicationDirectory inDomains:NSAllDomainsMask];
+            
+            for (NSURL *directory in appDirectories) 
+            {
+                NSString *directoryPath = [directory path];
+                BOOL fileExists = [fileManager fileExistsAtPath:directoryPath];
+                if (!fileExists) {
+                    NSLog(@"Skipping %@ because it does not exist.", directoryPath);
+                    continue;
+                }
+                
+                NSArray *propertiesToCache = [NSArray arrayWithObjects:NSURLNameKey, NSURLTypeIdentifierKey, nil];
+                NSDirectoryEnumerator *directoryEnumerator = [fileManager enumeratorAtURL:directory includingPropertiesForKeys:propertiesToCache options:NSDirectoryEnumerationSkipsPackageDescendants errorHandler:^BOOL(NSURL *url, NSError *error) {
                     
-                    BOOL isSandboxed = ([appBundle codeSignState] & OBCodeSignStateSandboxed) == OBCodeSignStateSandboxed;
-                    if (isSandboxed) {
-                        NSLog(@"%@ is sandboxed.", appName);
+                    NSLog(@"Error enumerating %@ in %@: %@", url, directory, error);
+                    return YES;
+                }];
+                
+                for (NSURL *url in directoryEnumerator) 
+                {
+                    NSDictionary *resourceValues = [url resourceValuesForKeys:propertiesToCache error:NULL];
+                    NSString *uti = [resourceValues objectForKey:NSURLTypeIdentifierKey];
+                    
+                    if ([uti isEqualToString:@"com.apple.application-bundle"]) 
+                    {
+                        NSString *appName = [resourceValues objectForKey:NSURLNameKey];
+                        NSBundle *appBundle = [[NSBundle alloc] initWithURL:url];
+                        
+                        if ([appBundle comesFromAppStore]) {
+                            NSLog(@"%@ is from the Mac App Store.", appName);
+                        }
+                        
+                        BOOL isSandboxed = ([appBundle codeSignState] & OBCodeSignStateSandboxed) == OBCodeSignStateSandboxed;
+                        if (isSandboxed) {
+                            NSLog(@"%@ is sandboxed.", appName);
+                        }
                     }
                 }
             }
         }
-        
     }
     return 0;
 }
